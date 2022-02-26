fb71d0c21f259e40607b4e401f4e6eb7f7ab7a9c
Cristian Gomez Taboada <cristian.gomez.taboada@hotmail.com>
2022-02-26 00:34:00 -0500

    Corrección URLs de Usuario

--- SED/apps/usuario/api/urls.py
+++ SED/apps/usuario/api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from apps.usuario.api.api import usuario_api_view, usuario_details_api_view
 
 urlpatterns = [
-    path("usuario/",usuario_api_view, name= "usuario_api"),
-    path("usuario/<str:codigo>/",usuario_details_api_view, name= "usuario_details_api_view") 
+    path("",usuario_api_view, name= "usuario_api"),
+    path("<str:codigo>/",usuario_details_api_view, name= "usuario_details_api_view") 
 ]
