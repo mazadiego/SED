8f8ee2c64d09421f1685e65175b54d51a7be1c81
Diego Maza <diegomazatapia@gmail.com>
2022-02-28 19:25:48 -0500

    restriccion llave foranea par institucion educativ

--- SED/apps/institucioneducativa/models.py
+++ SED/apps/institucioneducativa/models.py
@@ -4,7 +4,7 @@ from apps.usuario.models import Usuario
 class Institucioneducativa(models.Model):
     codigo = models.CharField(max_length=50,unique=True)
     nombre = models.CharField(max_length=500,null=False)
-    usuarioid = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    usuarioid = models.ForeignKey(Usuario, on_delete=models.RESTRICT,unique=True)
 
 def __str__(self):
     return f'{self.codigo} {self.nombre} {self.usuarioid}'
