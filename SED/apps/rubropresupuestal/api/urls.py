d6262c0b3558ba125d8d97e01a45289a0219b034
Diego Maza <diegomazatapia@gmail.com>
2022-05-16 14:45:41 -0500

    get saldo rubro x recaudo y x solicitud

--- SED/apps/rubropresupuestal/api/urls.py
+++ SED/apps/rubropresupuestal/api/urls.py
@@ -1,6 +1,6 @@
 from xml.etree.ElementInclude import include
 from django.urls import path
-from apps.rubropresupuestal.api.api import rubropresupuestal_api_view, rubropresupuestal_details_api_view,rubropresupuestal_final_api_view,saldorubroporproyeccion_final_api_view,rubropresupuestal_proyectados_api_view
+from apps.rubropresupuestal.api.api import rubropresupuestal_api_view, rubropresupuestal_details_api_view,rubropresupuestal_final_api_view,saldorubroporproyeccion_final_api_view,rubropresupuestal_proyectados_api_view,saldorubro_solicitud_api_view,saldorubro_recaudo_api_view
 
 
 urlpatterns = [
@@ -8,6 +8,7 @@ urlpatterns = [
     path("<str:codigo>/",rubropresupuestal_details_api_view,name="rubropresupuestal_details_api_view"),
     path("detalle/final/",rubropresupuestal_final_api_view,name="rubropresupuestal_final_api_view"),
     path("detalle/saldoporproyeccion/",saldorubroporproyeccion_final_api_view,name="saldorubroporproyeccion_final_api_view"),
-    path("detalle/proyectados/",rubropresupuestal_proyectados_api_view,name="rubropresupuestal_proyectados_api_view")
-    
+    path("detalle/proyectados/",rubropresupuestal_proyectados_api_view,name="rubropresupuestal_proyectados_api_view"),
+    path("detalle/saldosolicitud/",saldorubro_solicitud_api_view,name="saldorubro_solicitud_api_view"),
+    path("detalle/saldorecaudo/",saldorubro_recaudo_api_view,name="saldorubro_recaudo_api_view"),
 ]
