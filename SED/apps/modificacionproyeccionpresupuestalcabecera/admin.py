34960a81bbaa78b2294a0a55d61ac9b7dfd4fb92
Cristian Gomez Taboada <cristian.gomez.taboada@hotmail.com>
2022-08-03 20:39:22 -0500

    admin modificacion proyeccion

--- SED/apps/modificacionproyeccionpresupuestalcabecera/admin.py
+++ SED/apps/modificacionproyeccionpresupuestalcabecera/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Modificacionproyeccionpresupuestalcabecera
 
 # Register your models here.
+admin.site.register(Modificacionproyeccionpresupuestalcabecera)
